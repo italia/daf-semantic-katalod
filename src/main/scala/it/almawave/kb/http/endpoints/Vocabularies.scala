ac89fdba91254c8022374011816e1e18be366bbe
Alfredo Serafini <al.serafini@almaviva.it>
2018-11-06 18:33:22 +0100

    minor updates in query for DCAT-AP_IT, with latest kbaselib 0.0.10

--- src/main/scala/it/almawave/kb/http/endpoints/Vocabularies.scala
+++ src/main/scala/it/almawave/kb/http/endpoints/Vocabularies.scala
@@ -5,15 +5,15 @@ import java.nio.file.Paths
 
 import javax.inject.Inject
 import javax.ws.rs._
-import io.swagger.annotations.{Api, ApiOperation, ApiParam, Tag}
-import javax.ws.rs.core.{MediaType, Response}
+import io.swagger.annotations.{ Api, ApiOperation, ApiParam, Tag }
+import javax.ws.rs.core.{ MediaType, Response }
 import com.sun.net.httpserver.Authenticator.Success
 import com.typesafe.config.ConfigFactory
 import it.almawave.linkeddata.kb.catalog.models
 import it.almawave.linkeddata.kb.catalog.models.Hierarchy
 //import it.almawave.kb.http.models.Hierarchy
 import it.almawave.linkeddata.kb.catalog.models.VocabularyMeta
-import it.almawave.linkeddata.kb.catalog.{SPARQL, VocabularyBox}
+import it.almawave.linkeddata.kb.catalog.{ SPARQL, VocabularyBox }
 import it.almawave.linkeddata.kb.utils.JSONHelper
 import org.slf4j.LoggerFactory
 
@@ -31,7 +31,6 @@ class Vocabularies {
 
   @Inject
   var loader: CatalogService = null
-
 
   @GET
   @Produces(Array(MediaType.APPLICATION_JSON))
@@ -65,8 +64,9 @@ class Vocabularies {
   @Tag(name = "catalog, vocabularies")
   @ApiOperation(nickname = "vocabularyFlat", value = "details of a vocabulary custom")
   @Path("/flat/{vocabularyID}")
-  def detailsCustom(@PathParam("vocabularyID")@DefaultValue("licences") vocabularyID: String,
-                    @ApiParam(required = false)@QueryParam("lang")@DefaultValue("it") lang:String) = {
+  def detailsCustom(
+    @PathParam("vocabularyID")@DefaultValue("licences") vocabularyID:     String,
+    @ApiParam(required = false)@QueryParam("lang")@DefaultValue("it") lang:String) = {
 
     logger.debug(s"getting the details for vocabulary bis with id: ${vocabularyID} and lang: $lang")
     val vbox = loader.catalog.getVocabularyByID(vocabularyID).get
@@ -84,7 +84,7 @@ class Vocabularies {
     val voca = VocabularyBox.parse(skos_url)
     voca.start()
 
-    val concepts = SPARQL(voca.repo).query( lines.toStream.mkString )
+    val concepts = SPARQL(voca.repo).query(lines.toStream.mkString)
 
     voca.stop()
 
@@ -96,8 +96,9 @@ class Vocabularies {
   @Tag(name = "catalog, vocabularies")
   @ApiOperation(nickname = "vocabularyHierarchy", value = "details of a vocabulary custombis")
   @Path("/hierarchy/{vocabularyID}")
-  def detailsCustomBis(@PathParam("vocabularyID")@DefaultValue("licences") vocabularyID: String,
-                       @ApiParam(required = false)@QueryParam("lang")@DefaultValue("it") lang:String) = {
+  def detailsCustomBis(
+    @PathParam("vocabularyID")@DefaultValue("licences") vocabularyID:     String,
+    @ApiParam(required = false)@QueryParam("lang")@DefaultValue("it") lang:String) = {
 
     logger.debug(s"getting the details for vocabulary bis with id: ${vocabularyID} and lang: $lang")
 
@@ -117,16 +118,18 @@ class Vocabularies {
 
     voca.start()
 
-    val concepts = SPARQL(vbox.repo).query( lines.toStream.mkString )
+    val concepts = SPARQL(vbox.repo).query(lines.toStream.mkString)
     val hierarchy_broader = ListBuffer[Hierarchy]()
 
-    if(true) {
+    if (true) {
 
       concepts.toList
-        .foreach { item => if(!item.contains("parent_uri")) {
-          println("broader: \n" + item)
-          HierarchyUtility.read_concepts(scala.collection.mutable.Map(item.toSeq: _*), concepts, hierarchy_broader)
-        }
+        .foreach { item =>
+          if (!item.contains("parent_uri")) {
+            println("broader: \n" + item)
+            val map_item = scala.collection.mutable.Map(item.toSeq: _*)
+            HierarchyUtility.read_concepts(map_item, concepts, hierarchy_broader)
+          }
         }
     }
 
@@ -136,95 +139,94 @@ class Vocabularies {
 
   object HierarchyUtility {
 
-
     def setHierarchy(vMeta: VocabularyMeta) = {
 
-        logger.debug(s"getting the details for vocabulary bis with id: ${vMeta.id} and lang: ${vMeta.langs.headOption.getOrElse("")}")
+      logger.debug(s"getting the details for vocabulary bis with id: ${vMeta.id} and lang: ${vMeta.langs.headOption.getOrElse("")}")
 
-        val vbox = loader.catalog.getVocabularyByID(vMeta.id).get
-        val vboxid = vbox.meta.source
-        logger.debug("vboxid: " + vboxid)
+      val vbox = loader.catalog.getVocabularyByID(vMeta.id).get
+      val vboxid = vbox.meta.source
+      logger.debug("vboxid: " + vboxid)
 
-        val fileRdf = Paths.get(vboxid.toURI).toString
-        val skos_url = new URL(s"file:///$fileRdf")
+      val fileRdf = Paths.get(vboxid.toURI).toString
+      val skos_url = new URL(s"file:///$fileRdf")
 
-        val conf = ConfigFactory.parseFile(Paths.get("./conf/catalog.conf").normalize().toFile()).getConfig("daf.vocabularies")
-        val config_dir = Paths.get(conf.root().origin().filename()).toFile().getParent
-        val default_query_hierarchy: java.nio.file.Path = Paths.get(config_dir, conf.getString("query.hierarchy")).normalize().toAbsolutePath()
-        val lines: List[String] = Source.fromFile(default_query_hierarchy.toUri).getLines.toList
+      val conf = ConfigFactory.parseFile(Paths.get("./conf/catalog.conf").normalize().toFile()).getConfig("daf.vocabularies")
+      val config_dir = Paths.get(conf.root().origin().filename()).toFile().getParent
+      val default_query_hierarchy: java.nio.file.Path = Paths.get(config_dir, conf.getString("query.hierarchy")).normalize().toAbsolutePath()
+      val lines: List[String] = Source.fromFile(default_query_hierarchy.toUri).getLines.toList
 
-        val voca = VocabularyBox.parse(skos_url)
+      val voca = VocabularyBox.parse(skos_url)
 
-        voca.start()
+      voca.start()
 
-        val concepts = SPARQL(vbox.repo).query( lines.toStream.mkString )
-        vMeta.hierarchy.clear()
-        val hierarchy_broader: ListBuffer[Hierarchy] = vMeta.hierarchy
+      val concepts = SPARQL(vbox.repo).query(lines.toStream.mkString)
+      vMeta.hierarchy.clear()
+      val hierarchy_broader: ListBuffer[Hierarchy] = vMeta.hierarchy
 
-        if(true) {
+      if (true) {
 
-          concepts.toList
-            .foreach { item => if(!item.contains("parent_uri")) {
+        concepts.toList
+          .foreach { item =>
+            if (!item.contains("parent_uri")) {
               println("broader: \n" + item)
               HierarchyUtility.read_concepts(scala.collection.mutable.Map(item.toSeq: _*), concepts, hierarchy_broader)
             }
-            }
-        }
+          }
+      }
 
-        voca.stop()
-        hierarchy_broader
+      voca.stop()
+      hierarchy_broader
     }
 
-
-
-    def read_concepts(key_item : scala.collection.mutable.Map[String,Any],
-                      _concepts : Seq[Map[String,Any]], hierarchy_child: ListBuffer[Hierarchy] ) : ListBuffer[Hierarchy] = {
+    def read_concepts(
+      key_item:  scala.collection.mutable.Map[String, Any],
+      _concepts: Seq[Map[String, Any]], hierarchy_child: ListBuffer[Hierarchy]): ListBuffer[Hierarchy] = {
 
       val tmp_hierarchy_child = ListBuffer[Hierarchy]()
       hierarchy_child += convert_mutable_map_to_model(key_item)
       tmp_hierarchy_child += hierarchy_child.last
       var children = true
 
-      while(children) {
+      while (children) {
 
         //lista di collection temporanea
         tmp_hierarchy_child
           .toList.foreach { item_hierarchy =>
-          var tmp_hierarchy_child_sub = ListBuffer[Hierarchy]()
-          _concepts
-            .toList.foreach {
-            item =>
-              if (item.contains("parent_uri") & item.getOrElse("parent_uri", "").toString.equals(item_hierarchy.uri)) {
+            var tmp_hierarchy_child_sub = ListBuffer[Hierarchy]()
+            _concepts
+              .toList.foreach {
+                item =>
+                  if (item.contains("parent_uri") & item.getOrElse("parent_uri", "").toString.equals(item_hierarchy.uri)) {
 
-                tmp_hierarchy_child_sub += convert_mutable_map_to_model(scala.collection.mutable.Map(item.toSeq: _*))
+                    tmp_hierarchy_child_sub += convert_mutable_map_to_model(scala.collection.mutable.Map(item.toSeq: _*))
 
-                children = true
+                    children = true
+                  }
               }
-          }
 
-          // cocludendo la lettura di tutti i concetti aggiungo alla lista tmp gli elementi che hanno soddifatto la condizione
-          if(tmp_hierarchy_child_sub != null && tmp_hierarchy_child_sub.size > 0) {
-            tmp_hierarchy_child.clear()
-            tmp_hierarchy_child_sub.copyToBuffer(tmp_hierarchy_child)
-            addBroaderToList(item_hierarchy, tmp_hierarchy_child_sub)
-          }else
-            children = false
-        }
-      }//while
+            // cocludendo la lettura di tutti i concetti aggiungo alla lista tmp gli elementi che hanno soddifatto la condizione
+            if (tmp_hierarchy_child_sub != null && tmp_hierarchy_child_sub.size > 0) {
+              tmp_hierarchy_child.clear()
+              tmp_hierarchy_child_sub.copyToBuffer(tmp_hierarchy_child)
+              addBroaderToList(item_hierarchy, tmp_hierarchy_child_sub)
+            } else
+              children = false
+          }
+      } //while
 
       hierarchy_child
     }
 
-    def convert_mutable_map_to_model(item : scala.collection.mutable.Map[String,Any]) : Hierarchy = {
+    def convert_mutable_map_to_model(item: scala.collection.mutable.Map[String, Any]): Hierarchy = {
 
       val _hierarchy = Hierarchy(item.get("codice").get.toString, item.get("label").get.toString,
         item.get("uri").get.toString, item.getOrElse("parent_uri", "").toString, new ListBuffer[Hierarchy]())
       _hierarchy
     }
 
-    def addBroaderToList(_item_hierarchy: Hierarchy, listMem: ListBuffer[Hierarchy]) : Hierarchy = {
+    def addBroaderToList(_item_hierarchy: Hierarchy, listMem: ListBuffer[Hierarchy]): Hierarchy = {
 
-      if(listMem.toList.head.parent_uri.equals(_item_hierarchy.uri))
+      if (listMem.toList.head.parent_uri.equals(_item_hierarchy.uri))
         listMem.copyToBuffer(_item_hierarchy.children)
 
       _item_hierarchy
